780c363d68b3a97d4983040a435dfe2c7f860cea
perfectgbakidz <gbakidzsunday123@gmail.com>
2025-08-23 22:33:10 +0100

    fix: update main.py with recent changes

--- app/main.py
+++ app/main.py
@@ -1,27 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware  # <--- add this
-from .routers import auth, users, admin, admin_setup
-from . import models, database
+from .routers import auth, users, admin
+from . import  models, database
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="Optivus Backend")
 
-origins = [
-    "https://optivlive.onrender.com",  # your frontend URL
-    "http://localhost:3000"            # optional, for local dev
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # can also use ["*"] to allow all origins
+    allow_origins=["*"],  # can also use ["*"] to allow all origins
     allow_credentials=True,
     allow_methods=["*"],    # allow GET, POST, PUT, etc.
-    allow_headers=["*"],    # allow all headers
+    allow_headers=["*"], 
+    expose_headers=["*"],   # allow all headers
 )
 
 # Include Routers
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(admin.router)
-app.include_router(admin_setup.router)
+
